CM4all/libwas

commit c7734a0e8e53e3199aaa7cdc507dd03377cbfe48

simple: don't use memset() in _init_request()

src/simple.c
@@ -161,9 +161,13 @@ struct was_simple {
 static void
 was_simple_init_request(struct was_simple *w)
 {
-    memset(&w->request, 0, sizeof(w->request));
-
     w->request.method = HTTP_METHOD_GET;
+
+    w->request.uri = NULL;
+    w->request.script_name = NULL;
+    w->request.path_info = NULL;
+    w->request.query_string = NULL;
+
     w->request.headers = g_hash_table_new_full(g_str_hash, g_str_equal,
                                                g_free, g_free);
     w->request.parameters = g_hash_table_new_full(g_str_hash, g_str_equal,

commit 70ddaed7d5267e708fdea7e91f8713f120a43cbc

simple: clear "finished" in _init_request()
Fix for regression from c7734a0e

src/simple.c
@@ -173,6 +173,8 @@ was_simple_init_request(struct was_simple *w)
                                                g_free, g_free);
     w->request.parameters = g_hash_table_new_full(g_str_hash, g_str_equal,
                                                   g_free, g_free);
+
+    w->request.finished = false;
 }
 
 static void