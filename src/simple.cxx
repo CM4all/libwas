CM4all/libwas

commit 4283767226592e725821628e3c52ae218fb5b6ed

simple: check state==END in PollInput()
Fixes two more STOP handlers.

src/simple.cxx
@@ -941,6 +941,10 @@ was_simple::PollInput(int timeout_ms)
         return WAS_SIMPLE_POLL_ERROR;
     }
 
+    if (response.state == Response::State::END)
+        /* this may have been caused by STOP */
+        return WAS_SIMPLE_POLL_ERROR;
+
     /* check "eof" again, as it may have changed after control packets
        have been handled */
     if (input.IsEOF())
@@ -975,6 +979,10 @@ was_simple::PollInput(int timeout_ms)
                 response.state = Response::State::ERROR;
                 return WAS_SIMPLE_POLL_ERROR;
             }
+
+            if (response.state == Response::State::END)
+                /* this may have been caused by STOP */
+                return WAS_SIMPLE_POLL_ERROR;
 
             if (input.IsEOF())
                 return WAS_SIMPLE_POLL_END;

commit e6e81d5b501e7cc87648db88e27e30e69a3793b8

simple: check output.premature before state==END in PollInput()
Adds a missing check to the code from commit
4283767226592e725821628e3c52ae218fb5b6ed, which caused a regression,
because it made the process exit if it ever called PollInput() while
discarding the unused portion of the request body.

src/simple.cxx
@@ -941,7 +941,7 @@ was_simple::PollInput(int timeout_ms)
         return WAS_SIMPLE_POLL_ERROR;
     }
 
-    if (response.state == Response::State::END)
+    if (output.premature && response.state == Response::State::END)
         /* this may have been caused by STOP */
         return WAS_SIMPLE_POLL_ERROR;
 
@@ -980,7 +980,7 @@ was_simple::PollInput(int timeout_ms)
                 return WAS_SIMPLE_POLL_ERROR;
             }
 
-            if (response.state == Response::State::END)
+            if (output.premature && response.state == Response::State::END)
                 /* this may have been caused by STOP */
                 return WAS_SIMPLE_POLL_ERROR;
 