998397a3650fc04b32d654473d21bf9fe61e1104
Max Kellermann <mk@cm4all.com>
2017-09-01 12:40:08 +0200

    build with Meson and Ninja

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,3 @@
-#!/bin/sh
-
-set -e
-
-rm -rf config.cache build
-mkdir build
-libtoolize --force
-aclocal -I m4
-automake --add-missing --foreign
-autoconf
-./configure \
-	CFLAGS="-O0 -ggdb" CXXFLAGS="-O0 -ggdb" \
-        --prefix=/usr/local/stow/libcm4all-was \
-        --enable-debug \
-	--enable-silent-rules \
-        "$@"
+#!/bin/sh -e
+rm -rf build
+exec meson . build -Dprefix=/usr/local/stow/libcm4all-was --werror "$@"
